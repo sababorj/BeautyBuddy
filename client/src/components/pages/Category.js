0f545731f74456f196b141fd7045134f192337e7
Saba Schock <sababorjsaba@gmail.com>
2019-02-01 11:39:26 -0800

    category page draft

--- client/src/components/pages/Category.js
+++ client/src/components/pages/Category.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import AuthService from './AuthService';
+import type from '../../category.json'
 import { Link } from 'react-router-dom';
+import './style.css'
 
 class Category extends Component {
     constructor() {
         super();
         this.Auth = new AuthService();
     }
+ state={
+     type: type
+ }
 
-
+ 
     render() {
         return (
-            <div> category </div>
+            <div classNAme="row">
+            <div classNAme="col-6">
+                {this.state.type.map(item => (
+                    <>
+                <h1 className="category-tag">{item.name}</h1>
+                <div className="category-image" style={ {backgroundImage : `url(${item.image})`}}>
+                </div>
+                </>
+                ))}
+                </div>
+              </div>  
         );
     }
 }
