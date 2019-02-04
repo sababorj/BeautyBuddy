fc0129a83a4c8a3566cf60366610b17aed2a8071
Melanie Ryan <melaniehoperyan@gmail.com>
2019-02-04 12:38:06 -0800

    pushing profile frame

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -40,42 +40,25 @@ class Profile extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row card">
-          <div className="card-body col-sm drop-top card mx-auto bg-light mb-3">
+        <div className="row">
+          <div className="col-sm-3 drop-top card mx-auto bg-light mb-3">
             <img src={this.state.image} />
             <h5>Username: {this.state.username}</h5>
             <h5>{this.state.zipcode}</h5>
             <p>Favorite Brands:{this.state.favBrand}</p>
           </div>
-          <div className="col-6 ">
-            <div className="row drop-top card mx-auto bg-light"> saved
-
-            <div className="card" style="width: 18rem;">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-
-
-
-
-
-
-
+          <div className="col-md-9 shop-save">
+            <div className="row drop-top card mx-auto bg-light">
+              saved
             </div>
             <div className="row drop-top card mx-auto bg-light">
               shop
             </div>
           </div>
 
-          <div className="col-sm row drop-top card mx-auto bg-light">Google Places</div>
-
-
         </div>
 
+        <div className="row drop-top card mx-auto bg-light">Google Places</div>
 
 
       </div>
