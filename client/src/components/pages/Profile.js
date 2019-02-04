a7e1974a3d24a5945fbf4908a6a10eda2fe09362
Melanie Ryan <melaniehoperyan@gmail.com>
2019-02-04 13:08:20 -0800

    card-columns yeee

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -39,29 +39,56 @@ class Profile extends Component {
 
   render() {
     return (
-      <div className="container">
+      <div className="container-fluid">
         <div className="row">
-          <div className="col-sm-3 drop-top card mx-auto bg-light mb-3">
+        
+          <div className="col-sm-3 card mx-auto bg-light mb-3">
             <img src={this.state.image} />
             <h5>Username: {this.state.username}</h5>
             <h5>{this.state.zipcode}</h5>
             <p>Favorite Brands:{this.state.favBrand}</p>
           </div>
-          <div className="col-md-9 shop-save">
-            <div className="row drop-top card mx-auto bg-light">
-              saved
+
+
+          <div className="col-md-9 card-columns mx-auto bg-light">
+            <div className="card saved-rem">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">SAVED 1</p>
+              </div>
             </div>
-            <div className="row drop-top card mx-auto bg-light">
-              shop
+
+            <div className="card saved-rem">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">SAVED 2</p>
+              </div>
             </div>
+
+            <div className="card saved-rem">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">SAVED 3</p>
+              </div>
+            </div>
+
+
           </div>
 
+
+
+          <div className="row drop-top card mx-auto bg-light">
+            shop
+            </div>
         </div>
 
         <div className="row drop-top card mx-auto bg-light">Google Places</div>
 
 
       </div>
+
+
+
     )
   }
 }
