d966aa804d7e05b7ac788d602f547366d3896ea3
BettyWop <annettekalla1@gmail.com>
2019-02-05 19:30:20 -0800

    accepting incoming changes

--- client/src/components/pages/Item.js
+++ client/src/components/pages/Item.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
-import API from '../../utils/API';
-// import AuthService from './AuthService';
+import AuthService from './AuthService';
 
 class Item extends Component {
     constructor() {
         super();
-        // this.Auth = new AuthService();
-        this.state = {
-            items: []
-        }
+        this.Auth = new AuthService();
     }
 
     componentDidMount(){
-<<<<<<< HEAD
-        // console.log(this.props.location.state.productResult)
-=======
-        console.log(this.props)
-        API
-            .itemCall(this.props.location.state.productName || "Eye")
-            .then(res => {
-                console.log(res.data);
-            })
->>>>>>> ce213aae25004b93c44a7618ee0dad20f0617ec6
+        console.log(this.props.location.state.productResult)
     }
 
     render() {
