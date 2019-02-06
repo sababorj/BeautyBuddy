9b0449f0902b74361ce4bacede771dac152df599
catmblake <catmblake@gmail.com>
2019-02-06 11:33:27 -0800

    added secrets in dotenv

--- server.js
+++ server.js
@@ -170,7 +170,7 @@ app.post('/api/google/:zipcode', (req, res) => {
           let storesNearby = (response.data.results);
 
           // for loop through JSON response retrieve place info
-          for (let i = 0; i < 6; i++) {
+          for (let i = 0; i < 16; i++) {
             let store = {
               name: storesNearby[i].name,
               address: storesNearby[i].vicinity,
